dd566b6c603a936f07b5ba83b2e63de1062ecfd6
eumi72 <eumi72@gmail.com>
2026-04-24 17:01:23 +0200

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # per ora accettiamo tutto
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/health')
 def health():
     return {'status': 'ok'}
+
+@app.get("/ping")
+def ping():
+    return {"message": "pong"}
+
