5ddf254968a2051ea49a49feff5b0c804e259eff
eumi72 <eumi72@gmail.com>
2026-04-25 14:15:01 +0200

    Update healty.py

--- backend/routers/healty.py
+++ backend/routers/healty.py
@@ -2,6 +2,10 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
+@router.get("/health")
+def health():
+    return {"status": "ok"}
+
 @router.get("/ping")
 def ping():
     return {"message": "pong"}
